283394ce19ea212dc075895b311d46f7687110a5
Adamu Abubakar <engr.adamulab@gmail.com>
2024-12-08 18:08:36 +0100

    solve problem 1

---
+++ 01-index.js
@@ -0,0 +1,13 @@
+// PROBLEM 1 === Create an array with 5 students name, after that create a function which takes 2 parameters (allStudents & studentName), iterate over all students and find that specific user "studentName"
+
+const studentsDB = ["Adamu", "Abubakar", "Muhammad", "Idris", "Yusuf"];
+
+function students(allSTudents, studentName) {
+  for (let i = 0; i < allSTudents.length; i++) {
+    if (allSTudents[i] === studentName) {
+      console.log(`Found ${studentName}`);
+    }
+  }
+}
+
+students(studentsDB, "Idris");
